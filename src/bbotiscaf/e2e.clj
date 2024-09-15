572db57cdf8cbadfbbd92b96844191334192444c
Aleksandr Bogdanov <sasha_bogdanov_dev@yahoo.com>
2024-09-15 13:55:44 +0500

    e2e/get-flow-var

--- src/bbotiscaf/e2e.clj
+++ src/bbotiscaf/e2e.clj
@@ -3,6 +3,11 @@
     [bbotiscaf.impl.e2e.flow :as impl]))
 
 
+(defn get-flow-var
+  [k]
+  (k @impl/vars))
+
+
 (defn flow
   [flow-name ?data-from-flow blueprint]
   (impl/flow flow-name ?data-from-flow blueprint))
